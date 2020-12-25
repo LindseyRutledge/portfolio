8d35ef814ff92fde58486c6b1cc59785993fe0ae
LindseyRutledge <67494680+LindseyRutledge@users.noreply.github.com>
2020-12-25 12:02:16 -0800

    Added bells project to projects section

--- src/components/PortfolioPage/Sections/ProjectsSection.tsx
+++ src/components/PortfolioPage/Sections/ProjectsSection.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { RiGithubLine } from 'react-icons/ri';
+import { RiGithubLine, RiLinksLine } from 'react-icons/ri';
 import './ProjectsSection.scss';
 
 export const ProjectsSection: React.FunctionComponent = () => {
@@ -30,6 +30,46 @@ export const ProjectsSection: React.FunctionComponent = () => {
                             <li><em>Responsive Design</em></li>
                             <li><em>Themes</em> - Light/Dark</li>
                             <li><em>Scroll Indicator</em></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className='ProjectsSection-project'>
+                <div className='ProjectsSection-title'>
+                    <span className='ProjectsSection-link'>
+                        <span>
+                            <a href='https://github.com/LindseyRutledge/bells'>
+                                Bells
+                        </a>
+                        </span>
+                        <span>
+                            <a href="https://lindseyrutledge.github.io/bells/">
+                                <RiLinksLine className='ProjectsSection-linkIcon' />
+                            </a>
+                            <a href='https://github.com/LindseyRutledge/bells'>
+                                <RiGithubLine className='ProjectsSection-linkIcon' />
+                            </a>
+                        </span>
+                    </span>
+                </div>
+                <div className='ProjectsSection-description'>A simple application for "playing" hand bells.</div>
+                <div className='ProjectsSection-features'>
+                    <div className='ProjectsSection-features-section'>
+                        <div>Technologies:</div>
+                        <ul>
+                            <li><em>React</em></li>
+                            <li><em>TypeScript</em></li>
+                            <li><em>JavaScript</em></li>
+                            <li><em>SCSS</em>/<em>CSS3</em></li>
+                            <li><em>Github Pages</em></li>
+                        </ul>
+                    </div>
+                    <div className='ProjectsSection-features-section'>
+                        <div>Features:</div>
+                        <ul>
+                            <li>Lock note and <em>shake device</em> to ring bell (mobile)</li>
+                            <li>Press button to play bell sound</li>
+                            <li><em>Responsive Design</em></li>
                         </ul>
                     </div>
                 </div>
